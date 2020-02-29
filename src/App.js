4d38c02aee5c832e167ab5cdabbcda492ecc6555
lorexsan <lorenzosani97@gmail.com>
2020-02-29 17:15:57 +0000

    Add API call

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React, { Component } from 'react';
+import { request } from 'graphql-request';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    posts: {}
+  }
+
+  componentDidMount() {
+    const query = `{
+      allPosts(count: 1000) {
+        createdAt
+      }
+    }`;
+    request('https://fakerql.nplan.io/graphql', query)
+    .then((data) => {
+      this.setState({ posts: data })
+    })
+    .catch(console.log)
+  }
 
   render(){
     return (
